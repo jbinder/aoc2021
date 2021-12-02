84b147dd36df171011811e84d3e9e1d39d72e412
Johannes Binder <j.binder.x@gmail.com>
2021-12-02 08:39:55 +0100

    refactor d02

--- d02/main.py
+++ d02/main.py
@@ -1,23 +1,7 @@
-def d021():
-    with open("input.txt") as file:
-        pos = {'x': 0, 'y': 0}
-        pos_map = {'forward': (1, 0), 'up': (0, -1), 'down': (0, 1)}
-
-        for line in file:
-            command_segments = line.split(" ")
-            command = command_segments[0]
-            value = int(command_segments[1])
-            pos['x'] += pos_map[command][0] * value
-            pos['y'] += pos_map[command][1] * value
-
-        print(f"result: {pos['x'] * pos['y']}")
-
-
-def d022():
-    with open("input.txt") as file:
+def d02(input_file, action_map):
+    with open(input_file) as file:
         pos = {'x': 0, 'y': 0}
         aim = 0
-        action_map = {'forward': lambda v, a: (v, a * v, 0), 'up': lambda v, a: (0, 0, -v), 'down': lambda v, a: (0, 0, v)}
 
         for line in file:
             command_segments = line.split(" ")
@@ -28,8 +12,14 @@ def d022():
             pos['y'] += changes[1]
             aim += changes[2]
 
-        print(f"result: {pos['x'] * pos['y']}")
+        return pos['x'] * pos['y']
 
 
-d021()
-d022()
+action_map_1 = {'forward': lambda v, a: (v, 0, 0), 'up': lambda v, a: (0, -v, 0), 'down': lambda v, a: (0, v, 0)}
+action_map_2 = {'forward': lambda v, a: (v, a * v, 0), 'up': lambda v, a: (0, 0, -v), 'down': lambda v, a: (0, 0, v)}
+
+
+test_data = {'input_test.txt': (150, 900), 'input.txt': (1727835, 1544000595)}
+for file in test_data.keys():
+    print(d02(file, action_map_1) == test_data[file][0])
+    print(d02(file, action_map_2) == test_data[file][1])
